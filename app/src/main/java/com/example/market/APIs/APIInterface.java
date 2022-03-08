502a5b4c15f034c3e54da1413230d505cd4e7f76
ArsamHashmi <41957285+ArsamHashmi@users.noreply.github.com>
2022-03-08 12:33:40 +0500

    Update APIInterface.java

--- app/src/main/java/com/example/market/APIs/APIInterface.java
+++ app/src/main/java/com/example/market/APIs/APIInterface.java
@@ -10,7 +10,7 @@ import retrofit2.http.Query;
 public interface APIInterface {
 
     //https://pro.coinmarketcap.com/api/v1#section/Quick-Start-Guide
-    @Headers("X-CMC_PRO_API_KEY: 764066b5-18f4-4377-ba7c-70a0c988f5d3")
+    @Headers("X-CMC_PRO_API_KEY: ")
     @GET("/v1/cryptocurrency/quotes/latest?")
     Call<CryptoDetail> getCoinDetail(@Query("symbol") String coin);
 
